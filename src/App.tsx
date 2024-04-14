68c15eb698869f965f985422db364244d87b2f54
Sallam Rady <sallamrady@gmail.com>
2024-04-14 11:06:37 +0200

    create my react ts project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import MUITypography from "./components/MUITypography";
+
+function App() {
+  return (
+    <div className="App">
+      <MUITypography />
+    </div>
+  );
+}
+
+export default App;
